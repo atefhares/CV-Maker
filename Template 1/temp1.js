5d5bec5e4b7f5cb1e09c017851f16cb370be9821
Mohamed Atef <49479848+MohamedAtef11@users.noreply.github.com>
2020-02-14 17:21:17 +0200

    Template 1 update conditions

--- Template 1/temp1.js
+++ Template 1/temp1.js
@@ -18,7 +18,6 @@ var city=cvData.city;
 document.getElementById("add").textContent = city;
 
 var summary=cvData.summary;
-console.log(summary);
 document.getElementById("summary").textContent=summary;
 
 // this for skills
@@ -26,7 +25,7 @@ var skills=document.getElementById("skills");
 var levelskill=document.getElementById("levelskill");
 var skillsarr= cvData.skills;
 for(var i=0;i<skillsarr.length;i++){
-    skills.innerHTML+=`<b style="margin-left:10px;color:gray;">${skillsarr[i].name}</b><br>`;
+    skills.innerHTML+=`<b style="color:gray;">${skillsarr[i].name}</b><br>`;
     levelskill.innerHTML+=`<i style="margin-left:10px;color:#0DD95C;">${skillsarr[i].level}</i><br>`;
 }
 
@@ -40,8 +39,10 @@ for(var i=0;i<Interestsarr.length;i++){
 // this for education
 var education=document.getElementById("education");
 var degreearr= cvData.educations;
+if(degreearr.length == 0){
+    document.getElementById('eeducation').style.visibility = 'hidden'
+}
 for(var i=0;i<degreearr.length;i++){
-
     education.innerHTML+=`<h6 style="margin-left:5px;color:gray;font-size:18px;">${degreearr[i].educationDegree}</h6>`;
     education.innerHTML+=`<div style="margin-left:5px;color:red;font-size:16px;">${degreearr[i].schoolName}</div><br>`;
     education.innerHTML+=`<i style="margin-left:5px;color:gray;font-size:14px;">${degreearr[i].startDate} - ${degreearr[i].endDate}</i><br><br>`;
@@ -50,12 +51,16 @@ for(var i=0;i<degreearr.length;i++){
 // this for experiance
 var Experiences=document.getElementById("Experiences");
 var Experiencesarr= cvData.experances;
+
+if(Experiencesarr.length == 0){
+    document.getElementById('WExperiences').style.visibility = 'hidden'
+}
 for(var i=0;i<Experiencesarr.length;i++){
+   
+        Experiences.innerHTML+=`<h4 style="margin-left:10px;color:gray;">${Experiencesarr[i].jobTitle}</h4>`;
+        Experiences.innerHTML+=`<div style="width:100%"><p style="color:red;padding-left:10px;padding-left: 10px;float: left;margin-right: 10px"> ${Experiencesarr[i].companyName}</p><p style="margin-left:10px;color:gray;"> | ${Experiencesarr[i].startDate} - ${Experiencesarr[i].endDate}</p></div>`;
+        Experiences.innerHTML+=`<p style="margin-left:50px;margin-right:25px;color:gray;">${Experiencesarr[i].description}</p><br><br><br>`;
     
-    Experiences.innerHTML+=`<h4 style="margin-left:10px;color:gray;">${Experiencesarr[i].jobTitle}</h4>`;
-    Experiences.innerHTML+=`<p style="margin-left:370px;color:red;"> ${Experiencesarr[i].companyName}</p>`;
-    Experiences.innerHTML+=`<p style="margin-left:10px;color:gray;"> | ${Experiencesarr[i].startDate} - ${Experiencesarr[i].endDate}</p><br>`;
-    Experiences.innerHTML+=`<p style="margin-left:50px;margin-right:25px;color:gray;">${Experiencesarr[i].description}</p><br><br><br>`;
 }
 
 // this for languages
@@ -63,7 +68,7 @@ var languages=document.getElementById("languages");
 var languagesarr= cvData.languages;
 for(var i=0;i<languagesarr.length;i++){
     
-    languages.innerHTML+=`<h6 style="margin-left:10px;color:gray;">${languagesarr[i]}</h6>`;
+    languages.innerHTML+=`<h6 style="margin-left:10px;color:gray;">${languagesarr[i].name}</h6>`;
 }
 
 // >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
@@ -72,4 +77,5 @@ document.getElementById("print").addEventListener("click", function(){
     document.getElementById('print').style.visibility = 'hidden'
     window.print(); 
 }); 
+
 
